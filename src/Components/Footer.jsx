8b37934367f1dbdf9011814e0a9aecdd3ddb76cb
DarisYusyfa <yusyfadaris@gmail.com>
2023-11-17 02:00:16 +0700

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,8 +7,10 @@ export default function FooterComponent() {
       <footer className="bg-black">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="#" className="flex items-center mb-4 sm:mb-0">
+            <a href="#" className="grid grid-cols-1 gap-2 flex items-center mb-4 sm:mb-1">
               <img src="public/image/logoedu.png" alt="edutorlogo" />
+              <img src="public/image/sponsored.png" width={250} alt="edutorlogo" />
+
               <span className="self-center text-2xl font-semibold whitespace-nowrap text-white"></span>
             </a>
             <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6 xl:pb-12 lg:pb-10 md:pb-8 sm:pb-8 pb-8">
